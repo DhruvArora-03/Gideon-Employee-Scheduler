0d7bac8a62c41a56ffa94852337d119437e07948
DhruvArora-03 <93348668+DhruvArora-03@users.noreply.github.com>
2022-08-14 18:07:49 -0500

    downloaded new version of API and got azure database successfully connected

--- bot.py
+++ bot.py
@@ -4,9 +4,9 @@ API_TOKEN = '5350627481:AAH3m0Nw9JQJ9ShkoZaZ5T_TknXiZ9OUVwI'
 from bdb import effective
 import logging
 from telegram import Update
-from telegram.ext import filters, MessageHandler, ApplicationBuilder, CallbackContext, CommandHandler
+from telegram.ext import Filters, Updater, MessageHandler, CallbackContext, CommandHandler
 
-import database_handler as db_hand
+import db_handler
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -14,26 +14,26 @@ logging.basicConfig(
 )
 
 # handle clearChat command
-async def clearChat(update: Update, context: CallbackContext.DEFAULT_TYPE):
-    await context.bot.send_message(
+def clearChat(update: Update, context: CallbackContext):
+    context.bot.send_message(
         chat_id=update.effective_chat.id,
         text="clearing..." + "\n"*100 + "chat cleared"
     )
 
 # handle start command
-async def start(update: Update, context: CallbackContext.DEFAULT_TYPE):
-    await context.bot.send_message(
+def start(update: Update, context: CallbackContext):
+    context.bot.send_message(
         chat_id=update.effective_chat.id,
         text="Hey there, welcome to the team! Use the /help command for a list of commands."
     )
     print(update.effective_user.first_name)
     print(update.effective_user.last_name)
     print(update.effective_user.id)
-    db_hand.create_employee(update.effective_user.first_name, update.effective_user.last_name, update.effective_user.id)
+    db_handler.create_employee(update.effective_user.first_name, update.effective_user.last_name, update.effective_user.id)
 
 # handle help command
-async def help(update: Update, context: CallbackContext.DEFAULT_TYPE):
-    await context.bot.send_message(
+def help(update: Update, context: CallbackContext):
+    context.bot.send_message(
         chat_id=update.effective_chat.id,
         text=(
             '/settings - edit settings\n'
@@ -44,45 +44,49 @@ async def help(update: Update, context: CallbackContext.DEFAULT_TYPE):
     )
 
 # handle settings command
-async def settings(update: Update, context: CallbackContext.DEFAULT_TYPE):
-    await context.bot.send_message(
+def settings(update: Update, context: CallbackContext):
+    context.bot.send_message(
         chat_id=update.effective_chat.id,
         text="No editable settings yet"
     )
 
 # handle viewshifts command - TODO
-async def viewshifts(update: Update, context: CallbackContext.DEFAULT_TYPE):
+def viewshifts(update: Update, context: CallbackContext):
     # TODO: get shifts from database
     shifts = list()
 
     if len(shifts) == 0:
-        await context.bot.send_message(
+        context.bot.send_message(
             chat_id=update.effective_chat.id,
             text="You are not currently scheduled to work any upcoming shifts"
         )
     else:
-        await context.bot.send_message(
+        context.bot.send_message(
             chat_id=update.effective_chat.id,
             text="You are currently scheduled to work the following shifts:\n" + "\n".join(shifts)
         )
 
 # handle cancelshift command - TODO
-async def cancelshift(update: Update, context: CallbackContext.DEFAULT_TYPE):
-    await context.bot.send_message(
+def cancelshift(update: Update, context: CallbackContext):
+    context.bot.send_message(
         chat_id=update.effective_chat.id,
         text="Enter the ID number of the shift you want to cancel. Check out the /viewshifts command to find the ID number"
     )
 
 # handle non-command messages
-async def respond_to_non_command(update: Update, context: CallbackContext.DEFAULT_TYPE):
-    await context.bot.send_message(
+def respond_to_non_command(update: Update, context: CallbackContext):
+    context.bot.send_message(
         chat_id=update.effective_chat.id, 
         text="Sorry, I don't respond to non-command messages. Check out the /help command for a list of commands"
     )
 
 if __name__ == '__main__':
-    application = ApplicationBuilder().token(API_TOKEN).build()
-    
+    # application = Application.builder().token(API_TOKEN).build()
+    updater = Updater(API_TOKEN)
+
+    # Get the dispatcher to register handlers
+    dispatcher = updater.dispatcher
+
     # create handlers
     handlers = [
         CommandHandler('clearChat', clearChat),
@@ -91,14 +95,15 @@ if __name__ == '__main__':
         CommandHandler('settings', settings),
         CommandHandler('viewshifts', viewshifts),
         CommandHandler('cancelshift', cancelshift),
-        MessageHandler(filters.TEXT, respond_to_non_command), # only on msgs w no commands
+        MessageHandler(Filters.text & ~Filters.command, respond_to_non_command), # only on msgs w no commands
     ]
 
     # add handlers to application
     for handler in handlers:
-        application.add_handler(handler)
+        dispatcher.add_handler(handler)
     
 
-    application.run_polling(stop_signals=None)
+    updater.start_polling()
+    updater.idle()
 
 
--- db_handler.py
+++ db_handler.py
@@ -1,3 +1,4 @@
+from multiprocessing import current_process
 from venv import create
 from xml.dom.pulldom import END_ELEMENT
 import pyodbc
@@ -6,10 +7,14 @@ import pyodbc
 import warnings
 warnings.filterwarnings("ignore", category=DeprecationWarning) 
 
-# cnxn = pyodbc.connect(Trusted_Connection='yes', driver = '{SQL Server}',server = 'DHRUV-ALIENWARE\SQLEXPRESS' , database = 'GES')
+# cnxn = generate_db_connection()
+
+def generate_db_connection():
+    # return pyodbc.connect(Trusted_Connection='yes', driver = '{SQL Server}',server = 'DHRUV-ALIENWARE\SQLEXPRESS' , database = 'GES')
+    return pyodbc.connect(Trusted_Connection='no', driver='{ODBC Driver 17 for SQL Server}', server='gideon-employee-scheduler.database.windows.net', database='GES', uid='Gideon', pwd='{University380}')
 
 def get_all_employee_names():
-    cnxn = pyodbc.connect(Trusted_Connection='yes', driver = '{SQL Server}',server = 'DHRUV-ALIENWARE\SQLEXPRESS' , database = 'GES')
+    cnxn = generate_db_connection()
     cursor = cnxn.cursor()
     cursor.execute("SELECT * FROM Employees")
     employee_names = []
@@ -23,7 +28,7 @@ def get_all_employee_names():
     return employee_names
 
 def get_all_shifts_for_employee(id: int):
-    cnxn = pyodbc.connect(Trusted_Connection='yes', driver = '{SQL Server}',server = 'DHRUV-ALIENWARE\SQLEXPRESS' , database = 'GES')
+    cnxn = generate_db_connection()
     cursor = cnxn.cursor()
     cursor.execute("SELECT * FROM ShiftAssignments WHERE EmployeeID = ?", id)
     shifts = list()
@@ -38,14 +43,14 @@ def get_all_shifts_for_employee(id: int):
     return shifts
 
 def create_shift_with_IDs(date: str, empID: int, shiftID: int):
-    cnxn = pyodbc.connect(Trusted_Connection='yes', driver = '{SQL Server}',server = 'DHRUV-ALIENWARE\SQLEXPRESS' , database = 'GES')
+    cnxn = generate_db_connection()
     cursor = cnxn.cursor()
     cursor.execute("INSERT INTO ShiftAssignments(AssignmentDate, EmployeeID, ShiftID) VALUES (?,?,?)", date, empID, shiftID)
     cnxn.commit()
     cnxn.close()
 
 def create_shift_with_strings(date: str, empName: str, shiftTime: str):
-    cnxn = pyodbc.connect(Trusted_Connection='yes', driver = '{SQL Server}',server = 'DHRUV-ALIENWARE\SQLEXPRESS' , database = 'GES')
+    cnxn = generate_db_connection()
     cursor = cnxn.cursor()
     cursor.execute("SELECT EmployeeID FROM Employees WHERE FirstName = \'" + empName + "\'")
     empID = cursor.fetchone().EmployeeID
@@ -57,14 +62,14 @@ def create_shift_with_strings(date: str, empName: str, shiftTime: str):
 
 # this method creates a new employee with the specified parameters
 def create_employee(f_name: str, l_name: str, e_id: int):
-    cnxn = pyodbc.connect(Trusted_Connection='yes', driver = '{SQL Server}',server = 'DHRUV-ALIENWARE\SQLEXPRESS' , database = 'GES')
+    cnxn = generate_db_connection()
     cursor = cnxn.cursor()
     
     # first check if an employee with this ID already exists
     cursor.execute("SELECT * FROM Employees WHERE Employees.EmployeeID={}".format(e_id))
     
     if (cursor.fetchone() != None):
-        print('Employee with id {} already exists'.format(e_id))
+        print('Employee with id {} already exists under the name {} {}'.format(e_id, f_name, l_name))
     else:
         print('Creating employee...')
         cursor.execute("INSERT INTO Employees(EmployeeID, FirstName, LastName) VALUES ({}, '{}', '{}')".format(e_id, f_name, l_name))
@@ -72,11 +77,14 @@ def create_employee(f_name: str, l_name: str, e_id: int):
 
     cnxn.close()
 
-# cnxn = pyodbc.connect(Trusted_Connection='yes', driver = '{SQL Server}',server = 'DHRUV-ALIENWARE\SQLEXPRESS' , database = 'GES')
-# cursor = cnxn.cursor()
-# cursor.execute()
-# cnxn.commit()
+if (__name__ == '__main__'):
+    cnxn = generate_db_connection()
+    cursor = cnxn.cursor()
+    cursor.execute('INSERT INTO Employees (EmployeeID, FirstName, LastName) VALUES (1,\'d\',\'a\')')
+    cnxn.commit()
 
-# print(get_all_employee_names())
+    print(get_all_employee_names())
 
-# cnxn.close()
+    cursor.execute('DELETE FROM Employees WHERE Employees.FirstName=\'d\'')
+    cnxn.commit()
+    cnxn.close()

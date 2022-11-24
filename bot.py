c18abb1cdf1b2899229043ddce014ea3460eb528
DhruvArora-03 <93348668+DhruvArora-03@users.noreply.github.com>
2022-11-23 22:08:07 -0600

    adding in new bot commands

--- bot.py
+++ bot.py
@@ -1,7 +1,6 @@
 # telegram imports
 API_TOKEN = '5350627481:AAH3m0Nw9JQJ9ShkoZaZ5T_TknXiZ9OUVwI'
 
-from bdb import effective
 import logging
 from telegram import Update
 from telegram.ext import Filters, Updater, MessageHandler, CallbackContext, CommandHandler
@@ -42,54 +41,176 @@ def help(update: Update, context: CallbackContext):
             '/cancelshift - cancel a shift'
         )
     )
-
-# handle settings command
-def settings(update: Update, context: CallbackContext):
-    context.bot.send_message(
+    if db_handler.is_manager(update.effective_chat.id):
+        context.bot.send_message(
         chat_id=update.effective_chat.id,
-        text="No editable settings yet"
+        text=(
+            'Manager-only comamnds:\n\n'
+            '/createshift - create a new shift\n'
+            '/deleteshift - delete an existing shift'
+            '/populateshift - populate a shift with employees'
+            '/listshifts - list all shifts'
+        )
     )
 
-# handle viewshifts command
-def viewshifts(update: Update, context: CallbackContext):
-    shifts = db_handler.get_all_shifts_for_employee(update.effective_user.id)
+# # handle settings command
+# def settings(update: Update, context: CallbackContext):
+#     context.bot.send_message(
+#         chat_id=update.effective_chat.id,
+#         text="No editable settings yet"
+#     )
 
-    if len(shifts) == 0:
-        context.bot.send_message(
-            chat_id=update.effective_chat.id,
-            text="You are not currently scheduled to work any upcoming shifts"
-        )
-    else:
-        context.bot.send_message(
-            chat_id=update.effective_chat.id,
-            text="You are currently scheduled to work the following shifts:"
-        )
-        for shift in shifts:
-            context.bot.send_message(
-                chat_id=update.effective_chat.id,
-                text=shift
-            )
+# # handle viewshifts command
+# def viewshifts(update: Update, context: CallbackContext):
+#     shifts = db_handler.get_all_shifts_for_employee(update.effective_user.id)
+
+#     if len(shifts) == 0:
+#         context.bot.send_message(
+#             chat_id=update.effective_chat.id,
+#             text="You are not currently scheduled to work any upcoming shifts"
+#         )
+#     else:
+#         context.bot.send_message(
+#             chat_id=update.effective_chat.id,
+#             text="You are currently scheduled to work the following shifts:"
+#         )
+#         for shift in shifts:
+#             context.bot.send_message(
+#                 chat_id=update.effective_chat.id,
+#                 text=shift
+#             )
             
 
-# handle cancelshift command - TODO
-def cancelshift(update: Update, context: CallbackContext):
-    if len(context.args) == 0:
+# # handle cancelshift command - TODO
+# def cancelshift(update: Update, context: CallbackContext):
+#     if len(context.args) == 0:
+#         context.bot.send_message(
+#             chat_id=update.effective_chat.id,
+#             text="Cancel a shift by typing the command in this format: \n\"/cancelshift [DATE]\"\nFor instance, to cancel a shift for April 20th, 2022 you would say: \"/cancelshift 2022-04-20\""
+#         )
+#     else:
+#         if db_handler.delete_shift(context.args[0], update.effective_user.id):
+#             context.bot.send_message(
+#                 chat_id=update.effective_chat.id,
+#                 text="Shift successfully canceled."
+#             )
+#         else: 
+#             context.bot.send_message(
+#                 chat_id=update.effective_chat.id,
+#                 text="You are not scheduled for any shifts on {}.".format(context.args[0])
+
+# MANAGER ONLY COMMANDS:
+
+# handle promoteself command
+def promoteself(update: Update, context: CallbackContext):
+    if not db_handler.is_manager(update.effective_user.id):
+        db_handler.promote_employee(update.effective_user.id)
         context.bot.send_message(
             chat_id=update.effective_chat.id,
-            text="Cancel a shift by typing the command in this format: \n\"/cancelshift [DATE]\"\nFor instance, to cancel a shift for April 20th, 2022 you would say: \"/cancelshift 2022-04-20\""
+            text="You have been promoted to manager!"
         )
     else:
-        if db_handler.delete_shift(context.args[0], update.effective_user.id):
-            context.bot.send_message(
-                chat_id=update.effective_chat.id,
-                text="Shift successfully canceled."
+        context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text="You are already a manager."
+        )
+
+# handle createshift command
+def createshift(update: Update, context: CallbackContext):
+    '''
+    createshift: handle the /createshift command which asks the user for 7 params 
+                to create a new shift.
+                
+                /createshift [MONTH] [DAY] [YEAR] [HOUR] [MINUTE] [LENGTH] [# OF STAFF]
+
+    Args:
+        update (Update): _description_
+        context (CallbackContext): _description_
+    '''
+    if not db_handler.is_manager(update.effective_chat.id):
+        context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text='You do not have permission to run this command'
+        )
+    elif len(context.args) != 7: # month, day, year, hour, minute, length, staff
+        context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text=(
+                "Create a shift by typing the command in this format: \n\n"
+                "\"/createshift [MONTH] [DAY] [YEAR] [HOUR] [MINUTE] [SHIFT LENGTH] [# STAFF]\"\n\n"
+                "For instance, to create a shift for 4-20-2022, 3:30 PM to 7 PM, with 3 employees you would say: \n\n"
+                "\"/createshift 4 20 2022 15 30 3.5 3\""
             )
-        else: 
+        )
+    else:
+        args = [int(context.args[i]) if i != 5 else float(context.args[i]) for i in range(7)]
+        print('Creating shift with args:', args)
+        args.append(update.effective_user.id)
+        db_handler.create_shift(*args)
+        print('Shift created')
+        context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text="Shift successfully created."
+        )
+
+
+# handle deleteshift command
+def deleteshift(update: Update, context: CallbackContext):
+    pass # TODO
+
+# handle populateshift command
+def populateshift(update: Update, context: CallbackContext):
+    if not db_handler.is_manager(update.effective_chat.id):
+        context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text='You do not have permission to run this command'
+        )
+    elif len(context.args) != 1: # shift id
+        context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text=(
+                "Populate a shift by typing the command in this format: \n\n"
+                "\"/populateshift [SHIFT ID]\"\n\n"
+                "You can find shift IDs by running the /viewshifts command."
+            )
+        )
+    else:
+        shift_id = int(context.args[0])
+        if db_handler.populate_shift(shift_id):
             context.bot.send_message(
                 chat_id=update.effective_chat.id,
-                text="You are not scheduled for any shifts on {}.".format(context.args[0])
+                text="Shift successfully populated."
+            )
+        else:
+            context.bot.send_message(
+                chat_id=update.effective_chat.id,
+                text="Shift could not be populated. Use /shiftdetails to see more info."
             )
 
+# handle listshifts command
+def listshifts(update: Update, context: CallbackContext):
+    if not db_handler.is_manager(update.effective_chat.id):
+        context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text='You do not have permission to run this command'
+        )
+    else:
+        shifts = db_handler.get_all_shifts()
+        if len(shifts) == 0:
+            context.bot.send_message(
+                chat_id=update.effective_chat.id,
+                text="There are no upcoming shifts."
+            )
+        else:
+            context.bot.send_message(
+                chat_id=update.effective_chat.id,
+                text="The following shifts are currently scheduled:"
+            )
+            for shift in shifts:
+                context.bot.send_message(
+                    chat_id=update.effective_chat.id,
+                    text=shift
+                )
 
 # handle non-command messages
 def respond_to_non_command(update: Update, context: CallbackContext):
@@ -109,9 +230,12 @@ if __name__ == '__main__':
         CommandHandler('clearChat', clearChat),
         CommandHandler('start', start),
         CommandHandler('help', help),
-        CommandHandler('settings', settings),
-        CommandHandler('viewshifts', viewshifts),
-        CommandHandler('cancelshift', cancelshift),
+        # CommandHandler('settings', settings),
+        # CommandHandler('viewshifts', viewshifts),
+        # CommandHandler('cancelshift', cancelshift),
+        CommandHandler('promoteself', promoteself),
+        CommandHandler('createshift', createshift),
+        CommandHandler('populateshift', populateshift),
         MessageHandler(Filters.text & ~Filters.command, respond_to_non_command), # only on msgs w no commands
     ]
 
--- db_handler.py
+++ db_handler.py
@@ -1,6 +1,5 @@
-from multiprocessing import current_process
-from venv import create
-from xml.dom.pulldom import END_ELEMENT
+from datetime import datetime
+
 from pymongo import MongoClient
 
 CONNECTION_STRING = "mongodb+srv://Gideon:University380@ges.gjzwqlv.mongodb.net/?retryWrites=true&w=majority"
@@ -8,80 +7,16 @@ client = MongoClient(CONNECTION_STRING)
 db = client["GES"]
 
 
-# def get_all_employee_names():
-#     cnxn = generate_db_connection()
-#     cursor = cnxn.cursor()
-#     cursor.execute("SELECT * FROM Employees")
-#     employee_names = []
-#     while 1:
-#         row = cursor.fetchone()
-#         if not row:
-#             break
-#         employee_names.append(row.FirstName)
-#     cnxn.commit()
-#     cnxn.close()
-#     return employee_names
+'''
+create_employee: Create a new employee in the database
 
-def get_all_shifts_for_employee(id: int):
-    db.ShiftAssignments.find({'_id': id})
-    shiftIDs = []
-    row = cursor.fetchone()
-    while row:
-        shiftIDs.append(row.ShiftID)
-        row = cursor.fetchone()
-
-    shifts = []
-    for id in shiftIDs:
-        print('running command', "SELECT Shifts.DateTime FROM Shifts WHERE ShiftID =", id)
-        cursor.execute("SELECT Shifts.DateTime FROM Shifts WHERE ShiftID = ?", id)
-        shifts.append(str(cursor.fetchone().DateTime))
-    
-    cnxn.commit()
-    cnxn.close()
-    return shifts
-
-# def create_shift_with_IDs(date: str, empID: int, shiftID: int):
-#     cnxn = generate_db_connection()
-#     cursor = cnxn.cursor()
-#     cursor.execute("INSERT INTO ShiftAssignments(AssignmentDate, EmployeeID, ShiftID) VALUES (?,?,?)", date, empID, shiftID)
-#     cnxn.commit()
-#     cnxn.close()
-
-# def create_shift_with_strings(date: str, empName: str, shiftTime: str):
-#     cnxn = generate_db_connection()
-#     cursor = cnxn.cursor()
-#     cursor.execute("SELECT EmployeeID FROM Employees WHERE FirstName = \'" + empName + "\'")
-#     empID = cursor.fetchone().EmployeeID
-#     cursor.execute("SELECT ShiftID FROM Shifts WHERE ShiftTime = \'" + shiftTime + "\'")
-#     shiftID = cursor.fetchone().ShiftID
-#     cnxn.commit()
-#     cnxn.close()
-#     create_shift_with_IDs(date, empID, shiftID)
-
-# def delete_shift(shiftDate: str, empID: int):
-#     cnxn = generate_db_connection()
-#     cursor = cnxn.cursor()
-#     cursor.execute("SELECT Shifts.ShiftID FROM Shifts WHERE DATEDIFF(day, Shifts.DateTime, '{}') = 0".format(shiftDate))
-#     row = cursor.fetchone()
-#     if row:
-#         shiftID = row.ShiftID
-#         print('shiftID', shiftID)
-#         cursor.execute("SELECT * FROM ShiftAssignments WHERE ShiftAssignments.ShiftID = ? AND ShiftAssignments.EmployeeID = ?", shiftID, empID)
-#         row = cursor.fetchone()
-#         if row:
-#             cursor.execute("DELETE FROM ShiftAssignments WHERE ShiftAssignments.ShiftID = ? AND ShiftAssignments.EmployeeID = ?", shiftID, empID)
-#             cnxn.commit()
-#             cnxn.close()
-#             return True
-    
-
-#     cnxn.commit()
-#     cnxn.close()
-#     return False
-
-# this method creates a new employee with the specified parameters
-def create_employee(f_name: str, l_name: str, e_id: int):
-    existing = db.Employees.find_one({"_id": e_id})
+Args:
+    f_name (str): The employee's first name
+    l_name (str): The employee's last name
+    id (int): The employee's id #
+'''
+def create_employee(f_name: str, l_name: str, id: int):
+    existing = db.Employees.find_one({"_id": id})
 
     if (existing != None):
         print('Employee with id {} already exists under the name {} {}'.format(
@@ -91,18 +26,60 @@ def create_employee(f_name: str, l_name: str, e_id: int):
         ))
     else:
         print('Creating employee...')
-        employee = {'_id': e_id, 'first_name': f_name, 'last_name': l_name}
+        employee = {'_id': id, 'first_name': f_name, 'last_name': l_name, 'manager': False}
         db.Employees.update_one(employee, {'$set': employee}, upsert=True)
         print('Employee created successfully')
 
-# if (__name__ == '__main__'):
-    # cnxn = generate_db_connection()
-    # cursor = cnxn.cursor()
-    # cursor.execute('INSERT INTO Employees (EmployeeID, FirstName, LastName) VALUES (1,\'d\',\'a\')')
-    # cnxn.commit()
 
-    # print(get_all_employee_names())
+def get_all_shifts_for_employee(id: int):
+    return db.ShiftAssignments.find({'_id': id, 'manager': False}) != None
 
-    # cursor.execute('DELETE FROM Employees WHERE Employees.FirstName=\'d\'')
-    # cnxn.commit()
-    # cnxn.close()
+def is_manager(id: int):
+    return db.Employees.find_one({'_id': id}) != None
+
+def promote_employee(id: int):
+    employee = db.Employees.find_one({'_id': id})
+    if employee != None:
+        employee['manager'] = True
+        db.Employees.update_one(employee, {'$set': employee}, upsert=True)
+    else:
+        print('Employee with id {} does not exist'.format(id))
+
+def create_shift(month: int, day: int, year: int, hour: int, minute: int, 
+                length: float, num_staff: int, creator_id: int):
+    id = int('{0}{1}{2}{3}{4:02d}'.format(year - 2000, month, day, hour, minute))
+    dt = datetime(year, month, day, hour, minute)
+
+    existing = db.Shifts.find_one({"_id": id})
+    if existing != None:
+        print('Shift with id {} already exists'.format(id))
+        db.Shifts.delete_one(existing)
+        print('Existing Shift deleted')
+    shift = {
+        '_id': id,
+        'creator_id': creator_id,
+        'date_time': dt,
+        'length': length,
+        'num_staff': num_staff
+    }
+    db.Shifts.update_one(shift, {'$set': shift}, upsert=True)
+
+def delete_shift(id: int):
+    shift = db.Shifts.find_one({'_id': id})
+    if shift != None:
+        db.Shifts.delete_one(shift)
+        print('Shift deleted successfully')
+        return True
+    else:
+        print('Shift with id {} does not exist'.format(id))
+        return False
+
+def populate_shift(id: int):
+    shift = db.Shifts.find_one({'_id': id})
+    if shift != None:
+        for _ in range(shift['num_staff']):
+            assignment = {
+                '_id': id,
+                'employee_id': employee_id
+            }
+            db.ShiftAssignments.update_one(assignment, {'$set': assignment}, upsert=True)
